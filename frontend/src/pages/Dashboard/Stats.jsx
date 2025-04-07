6dab2f095fcb66b9abacaa70770c2119a608b47f
Nelson <chnelson91@gmail.com>
2025-04-07 23:18:10 +0200

    makes better

--- frontend/src/pages/Dashboard/Stats.jsx
+++ frontend/src/pages/Dashboard/Stats.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import DashboardLayout from "../../components/layouts/DashboardLayout";
 import ReportOverView from "../../components/Report/ReportOverView";
 
-const SetupBudget = ({ expenseHistory }) => {
+const SetupBudget = ({ income }) => {
   return (
     <DashboardLayout activeMenu="Stats">
       <div className="my-5 mx-auto">
         <div className="grid grid-cols-1 gap-6">
-          <ReportOverView
-           expenseHistory={expenseHistory}
+          <BudgetOverView
+            income={income}
           />
         </div>
       </div>
