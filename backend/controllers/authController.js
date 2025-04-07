1dd7c5bb42f1f52934d8f777f56cec618a670205
Nelson <chnelson91@gmail.com>
2025-04-07 16:54:36 +0200

    Add User model

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/user');
+const User = require('../models/User');
 
 const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1h" });
